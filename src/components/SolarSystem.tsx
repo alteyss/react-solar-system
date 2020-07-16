cdf87631835397757ee892caa226293e804c7d8d
Florent Teyssou <fteyssou@sinfin.fr>
2020-07-16 23:13:22 +0200

    Changed to use em

--- src/components/SolarSystem.tsx
+++ src/components/SolarSystem.tsx
@@ -4,41 +4,42 @@ import '../styles/index.scss';
 import { Mercury, Neptune, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Sun } from '../constants';
 
 const BASE_PERIOD   = 10;
+const MAX_SIZE      = 50;
 
-const BASE_DISTANCE = 3.5;
-const MAX_DISTANCE  = 52.5;
+const BASE_RADIUS   = 0.3;
+const MAX_RADIUS    = 1.3;
 
-const BASE_RADIUS   = 0.5;
-const MAX_RADIUS    = 1.25;
-
-const ORBIT_UNIT    = 'px'; 
-const OBJECT_UNIT   = 'rem'; 
-const SUN_UNIT      = 'px'; 
-const WIDTH         = 200;
+const ORBIT_UNIT    = 'em'; 
+const OBJECT_UNIT   = 'em'; 
+const SUN_UNIT      = 'em'; 
 
 export class SolarSystem extends React.Component {
 
+    state = {
+        width: 600
+    };
+
     getOrbitSize(distance: number): number {
         let squareRatio = Math.sqrt(distance / Neptune.DistanceFromSun);
-        // return Math.sqrt(distance / Neptune.DistanceFromSun) * MAX_DISTANCE + BASE_DISTANCE;
-        return WIDTH * squareRatio;
+        return squareRatio * MAX_SIZE;
     }
 
     getObjectSize(radius: number): number {
-        return (radius / Jupiter.Radius) * MAX_RADIUS + BASE_RADIUS;
+        let squareRatio = radius / Jupiter.Radius;
+        return squareRatio * MAX_RADIUS + BASE_RADIUS;
     }
 
     getSunSize(): number {
         let mercuryOrbitSize = this.getOrbitSize(Mercury.DistanceFromSun);
-        return (mercuryOrbitSize / 2);
+        return mercuryOrbitSize / 1.7;
     }
 
-    getOrbitStyles(lengthOfYear: number, distance: number, zIndex: number) {
+    getOrbitStyles(lengthOfYear: number, distance: number, elevation: number) {
         return {
             animationDuration: `${BASE_PERIOD * lengthOfYear}s`,
             height: `${this.getOrbitSize(distance)}${ORBIT_UNIT}`,
             width: `${this.getOrbitSize(distance)}${ORBIT_UNIT}`,
-            zIndex
+            zIndex: elevation
         };
     }
 
@@ -75,7 +76,15 @@ export class SolarSystem extends React.Component {
         const SunObject = this.getSunStyles();
 
         return (
-            <div className="solar-system">
+            <div className="solar-system" style={{
+                fontSize: (2 * this.state.width) / 100 // 2% of the width
+            }}>
+                <div className="commands">
+                    <input type="number" value={this.state.width} onInput={(val: any) => {
+                        this.setState({ width: val.target.value });
+                    }} />
+                </div>
+
                 <div className='orbit sun'>
                     <SunObject className='object' />
                 </div>
